756206a1530100b573fb0614ee70d3cf188a8521
konstantin.v <7770hunter0777@gmail.com>
2023-07-21 14:48:00 +0800

    add new comment

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -42,7 +42,7 @@ public class UserController {
         users.put(user.getName(), user);
         return user;
     }
-
+    // changes
     @PutMapping
     public User changeAge(@RequestBody User user) {
         users.put(user.getName(), user);
